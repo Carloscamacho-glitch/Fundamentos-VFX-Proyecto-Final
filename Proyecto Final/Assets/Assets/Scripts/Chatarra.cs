e4684bcd0654f16dec8376e8d3c806e90f4f11e0
Carlos Camacho <carloscamacho.cm96@gmail.com>
2025-10-20 22:44:50 -0600

    Estructuras, enemigos y mejora en la nave
    Se agregaron las estructuras a la ciudad abandonada y se colocaron los enemigos en la misma y en la ciudad principal. Además se mejoro el código de reparación de la nave

--- Proyecto Final/Assets/Assets/Scripts/Chatarra.cs
+++ Proyecto Final/Assets/Assets/Scripts/Chatarra.cs
@@ -2,7 +2,15 @@ using UnityEngine;
 
 public class Chatarra : MonoBehaviour
 {
-    [SerializeField] private float rotationSpeed = 90f;
+    [SerializeField] private float rotationSpeed = 90f; // velocidad de rotación
+    [SerializeField] private float cooldownTiempo; // tiempo de espera antes de poder recoger
+    private bool puedeRecogerse = false; // controla si puede ser recogida
+
+    private void Start()
+    {
+        // Comienza el cooldown
+        StartCoroutine(ActivarRecogidaDespuesDeTiempo());
+    }
 
     private void Update()
     {
@@ -12,12 +20,15 @@ public class Chatarra : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
+        if (!puedeRecogerse) return;
+
+        // Solo responde si el que colisiona es el jugador
         if (!collision.gameObject.CompareTag("Player")) return;
 
         // Cantidad aleatoria entre 1 y 10
         int cantidadChatarra = Random.Range(1, 11);
 
-        // Accedemos al componente del jugador que gestiona la chatarra
+
         PlayerController player = collision.gameObject.GetComponent<PlayerController>();
         if (player != null)
         {
@@ -26,5 +37,12 @@ public class Chatarra : MonoBehaviour
 
         // Destruimos el objeto tras recogerlo
         Destroy(gameObject);
+    }
+
+    private System.Collections.IEnumerator ActivarRecogidaDespuesDeTiempo()
+    {
+        // Espera 1 segundos antes de permitir la recogida
+        yield return new WaitForSeconds(cooldownTiempo);
+        puedeRecogerse = true;
     }
 }
--- Proyecto Final/Assets/Assets/Scripts/NaveController.cs
+++ Proyecto Final/Assets/Assets/Scripts/NaveController.cs
@@ -3,26 +3,52 @@ using UnityEngine;
 public class NaveController : MonoBehaviour
 {
     [Header("Referencias de Objetos")]
-    [SerializeField] private GameObject naveRota;               // la nave rota en el escenario
-    [SerializeField] private GameObject nave;                   // la nave reparada que se activará
+    [SerializeField] private GameObject naveRota; // Nave dañada
+    [SerializeField] private GameObject nave;     // Nave reparada
+    [SerializeField] private float rangoDeteccion; // Distancia para activar reparación
+    private bool naverReparada = false;
 
-    private void OnCollisionEnter(Collision collision)
+    [SerializeField] private Transform jugador;
+
+    void Update()
     {
-        if (!collision.gameObject.CompareTag("Player")) return;
+        if (jugador == null) return;
 
-        PlayerController player = collision.gameObject.GetComponent<PlayerController>();
-        if (player == null) return;
+        PlayerController player = jugador.GetComponent<PlayerController>();
 
-        if (player.TieneMotor() && player.GetMateriales() >= 2)
+        // Calcular distancia al jugador
+        float distancia = Vector3.Distance(transform.position, jugador.position);
+
+        // Si está dentro del rango de detección
+        if (distancia <= rangoDeteccion )
         {
-            if (naveRota != null) naveRota.SetActive(false);
-            if (nave != null) nave.SetActive(true);
+            if (Input.GetKey(KeyCode.E))
+            {
+                if (player.TieneMotor() && player.GetMateriales() >= 2 && !naverReparada)
+                {
+                    RepararNave();
+                    naverReparada = true;
+                }
+                else if (naverReparada)
+                {
+                    return;
+                }
+                else
+                {
+                    Debug.Log("No tienes los materiales o el motor necesario para reparar la nave."); // poner en interfaz después
+                }
+            }else
+            {
+                Debug.Log("Presiona 'E' para reparar la nave.");    // poner en interfaz después
+            }
+        }
+    }
 
-            Debug.Log("La nave ha sido reparada y activada correctamente.");
-        }
-        else
-        {
-            Debug.Log("No tienes los materiales o el motor necesarios para reparar la nave.");
-        }
+    private void RepararNave()
+    {
+        Debug.Log("¡La nave ha sido reparada!"); // poner en interfaz después
+
+        naveRota.SetActive(false);
+        nave.SetActive(true);
     }
 }
